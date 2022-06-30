9fb3cca3a018e2ae85c484130a45c2ec832a99a1
David Hruschka <david-bruno.hruschka@tu-ilmenau.de>
2022-06-30 19:43:07 +0200

    Add startup script

---
+++ start_codeocean_dockercontainerpool.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# A small script to start codeocean and dockercontainerpool on two tmux panes in the same window.
+# Must be called from within the vagrant VM.
+sessionname='codeocean-session'
+windowname='codeocean'
+tmux new-session -d -s $sessionname
+tmux new-window -n $windowname -t 0 -k 'cd /home/vagrant/dockercontainerpool && rails s -p 7100'
+tmux split-window -h -b -t $windowname 'cd /home/vagrant/codeocean && rails s -b 0.0.0.0 -p 7000'
+tmux attach -t $sessionname
+
