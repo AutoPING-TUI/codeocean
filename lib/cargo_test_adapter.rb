fea99af275c6bd6aa0afb15259204ab6d1f3f08a
David Hruschka <david-bruno.hruschka@tu-ilmenau.de>
2022-07-08 16:03:10 +0200

    Show if the program could not be compiled.

--- lib/cargo_test_adapter.rb
+++ lib/cargo_test_adapter.rb
@@ -29,22 +29,33 @@ class CargoTestAdapter < TestingFrameworkAdapter
     end
 
     def extract_test_result_parameters(parsed_json_objects)
+        compile_success = false
         count = 0
         failed = 0
         error_messages = []
 
         parsed_json_objects.each { |obj|
-            if obj["type"] == "suite"
-                if obj.has_key?("test_count")
-                    count += obj["test_count"]
-                elsif obj.has_key?("failed")
-                    failed += obj["failed"]
-                end
-            end
-            if obj["type"] == "test" && obj["event"] == "failed"
-                error_messages.push(pretty_format(obj))
+            if obj["reason"] == "build-finished" && obj.has_key?("success")
+                compile_success = obj["success"]
             end
         }
+
+        if compile_success
+            parsed_json_objects.each { |obj|
+                if obj["type"] == "suite"
+                    if obj.has_key?("test_count")
+                        count += obj["test_count"]
+                    elsif obj.has_key?("failed")
+                        failed += obj["failed"]
+                    end
+                end
+                if obj["type"] == "test" && obj["event"] == "failed"
+                    error_messages.push(pretty_format(obj))
+                end
+            }
+        else
+            error_messages.push("Could not compile. See below for more details.")
+        end
 
         {count: count, failed: failed, error_messages: error_messages}
     end
